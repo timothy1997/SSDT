868366e2998155f94cd1fa6596d6388698d7ea33
Tim Sherry <timsherry97@gmail.com>
2018-06-10 14:41:24 -0400

    Add some new stuff I don't feel like documenting right now

--- sockets.py
+++ sockets.py
@@ -2,60 +2,51 @@
 
 import sys
 import socket
-import SocketsHeader
+import math
 
-class MySocket:
-    # For demonstration only, not a great implementation
-    def __init__(self, sock=None):
-        if sock is None:
-            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        else:
-            self.sock = sock
-
-    def connect(self, host, port):
-        self.sock.connect((host, port))
-
-    def mysend(self, msg):
-        totalsent = 0
-        while totalsent < MSGLEN:
-            sent = self.sock.send(msg[totalsent:])
-            if sent == 0:
-                raise RuntimeError("socket connection broken")
-            totalsent = totalsent + sent
-
-    def myreceive(self):
-        chunks = []
-        bytes_recd = 0
-        while bytes_recd < MSGLEN:
-            chunk = self.sock.recv(min(MSGLEN - bytes_recd, 2048))
-            if chunk == b'':
-                raise RuntimeError("socket connection broken")
-            chunks.append(chunk)
-            bytes_recd = bytes_recd + len(chunk)
-        return b''.join(chunks)
+encoding = 'utf-8'
 
 def Main():
     # Ask what the user wants to do...
     response = int(input("0.) Quit\n1.) Host a conversation\n2.) Connect to conversation\n"))
 
     if response == 1:
-        socket1 = MySocket()
-        # Create an INET (IPv4), STREAMing (TCP) socket
+        # Create an INET (IPv4), STREAMing (TCP) socket.
         serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         # bind the socket to a public host, and a well-known port
-        serversocket.bind((socket.gethostname(), 65534))
-        # "become" a socket
+        # if we binded to 'localhost' or '127.0.0.1', we would still have
+        # a server socket, but only visible within the same machine
+        # s.bind('', 80) specifies that the socket is reachable by any
+        # address the machine happens to have
+        serversocket.bind((socket.gethostname(), 8080))
+        # "become" a socket, that is listening for 5 connections
         serversocket.listen(5)
         # accept a connection from outside
         (clientsocket, address) = serversocket.accept()
-        serversocket.close()
-        print(clientsocket)
-        print(address)
+        # get the size of the message, and compute how many chunks that will be
+        message = clientsocket.recv(32)
+        message = message.decode(encoding)
+        chunks = math.ceil(int(message) / 8192)
+        # send to the client that the message was received, and that they may begin sending
+        clientsocket.send('1'.encode(encoding))
+        clientsocket.close()
     elif response == 2:
         hostname = input("Hostname: ")
-        socket2 = MySocket()
-        socket2.connect(hostname, 65534)
-        socket2.close()
+        message = input("Message Size: ")
+        # Create a client socket
+        clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # Connect to the serversocket
+        clientsocket.connect((hostname, 8080))
+        # Send them the data
+        clientsocket.send(message.encode(encoding))
+        verification = clientsocket.recv(1)
+        str(verification.decode(encoding))
+        print(verification)
+        if verification == '1':
+            print("Verification received, beginning message transmition sequence...")
+        else:
+            print("Some error occured...")
+        clientsocket.close()
     else:
         return
 
