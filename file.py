47227f88d9193e16bb37d40b3bc5657bda725588
Tim Sherry <timsherry97@gmail.com>
2018-06-14 23:34:47 -0400

    Added saving message to file

---
+++ file.py
@@ -0,0 +1,5 @@
+import sys
+
+filename = "test.txt"
+with open(filename, 'r') as content_file:
+    content = content_file.read()
--- sockets.py
+++ sockets.py
@@ -3,69 +3,74 @@
 import sys
 import socket
 import math
+import os
 
 # global values
 encoding = 'utf-8'
-chunksize = 8
+chunksize = 4096
 
 def Main():
     # Ask what the user wants to do...
     response = int(input('0.) Quit\n1.) Host a conversation\n2.) Connect to conversation\n'))
 
-    if response == 1:
-        # Create an INET (IPv4), STREAMing (TCP) socket.
+    if response == 1: # (Host)
+        # What's happening here:
+        # Create an INET (IPv4), STREAMing (TCP) socket, and bind the socket to a public host, and a
+        # well-known port. If we binded to 'localhost' or '127.0.0.1', we would still have a server
+        # socket, but only visible within the same machine. s.bind('', 80) specifies that the socket
+        # is reachable by any address the machine happens to have. The socket will listen for 5
+        # connections (eventually I want to implement this part using threads), and accept one. At
+        # this point, it will receive the size of the message by the user, and compute how many chunks
+        # that will be. It will then send a verification to the client, and the client will start sending
+        # the message. We first receive the name of the file, then we receive the chunks, decode them,
+        # save them in an array, then join them, outputing the message.
         serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        # bind the socket to a public host, and a well-known port
-        # if we binded to 'localhost' or '127.0.0.1', we would still have
-        # a server socket, but only visible within the same machine
-        # s.bind('', 80) specifies that the socket is reachable by any
-        # address the machine happens to have
         serversocket.bind((socket.gethostname(), 8080))
-        # "become" a socket, that is listening for 5 connections
         serversocket.listen(5)
-        # accept a connection from outside
         (clientsocket, address) = serversocket.accept()
-        # get the size of the message, decode it, and compute how many chunks that will be
+        print("Connection made...\n")
         message = clientsocket.recv(32)
         message = message.decode(encoding)
         chunksnum = math.ceil(int(message) / chunksize)
-        # send to the client that the message was received, and that they may begin sending
         clientsocket.send('1'.encode(encoding))
-        # begin receiving the message
-        # The total number of chunks
-        currentchunk = 0
+        filename = ''
         chunks = []
+        currentchunk = 0
+        filename = clientsocket.recv(chunksize).decode()
         while currentchunk < chunksnum:
-            chunk = clientsocket.recv(chunksize)
+            chunk = clientsocket.recv(chunksize).decode(encoding)
             chunks.append(chunk)
             currentchunk += 1
-        i = 0
-        while i < len(chunks):
-            chunks[i] = chunks[i].decode(encoding)
-            i += 1
-        clientsocket.close()
         finalMessage = "".join(chunks)
-        print(finalMessage)
-    elif response == 2:
-        # hostname = input('Hostname: ')
-        # message = input('Message: ')
-        hostname = 'tim-Inspiron-7352'
-        message = 'This is a message to all the people on earth. My name is Tim Sherry.'
-        messagesize = len(message)
-        # Create a client socket
+        clientsocket.close()
+        # Now we will output the file transferred in its own directory
+        os.makedirs(address[0])
+        f = open(address[0] + '/' + filename, 'w+')
+        f.write(finalMessage)
+    elif response == 2: # (Client)
+        # What's going on here: We get the hostname and the file to be sent, then we read the contents
+        # of the file. We create a socket that utilizes an IPv4, TCP connection, and send them the size
+        # of the file (which is info that they will need). We receive a verification message, we indicates
+        # whether they received the message or not. If they did, we send them the entire message
+        hostname = input('Hostname: ')
+        filename = input('File: ')
+        message = ''
+        with open(filename, 'r') as content_file:
+            message = content_file.read()
         clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        # Connect to the serversocket
         clientsocket.connect((hostname, 8080))
-        # Send them the data
-        clientsocket.send(str(messagesize).encode(encoding))
-        # Receive the verification
+        clientsocket.send(str(len(message)).encode(encoding))
         verification = clientsocket.recv(1)
         verification = verification.decode(encoding)
-        # If the verification message is one, begin transmitting the message. Otherwise, don't.
         if verification == '1':
-            # The total number of chunks
-            chunksnum = math.ceil(float(messagesize) / chunksize)
+            # What's going on here: We calculate the number of chunks we will send, and send the name
+            # of the file. Then we begin sending the actual message. While there are still chunks to
+            # send (currentchunk keeps track) of the chunks sent), we must send them. If the current
+            # chunk to be sent is not the last one, you send 4096 bytes. Otherwise, we just send the
+            # rest of the bytes. If 0 is returned, then something went wrong.
+            chunksnum = math.ceil(float(len(message)) / chunksize)
             currentchunk = 0
+            clientsocket.send(filename.encode(encoding))
             while currentchunk < chunksnum:
                 if currentchunk < (chunksnum - 1):
                     tosend = message[currentchunk*chunksize:(currentchunk*chunksize)+chunksize]
